94b38660b6b1b8deeb1adaf9ade9d5238d4fefd6
Malcolm_Leake <echelon1089@gmail.com>
2020-09-30 23:33:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl'
 import './App.css';
+import Main from './components/main';
+import {Link} from 'react-router-dom'
+import resume from './components/Resume.docx'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title="Title" scroll>
+            <Navigation>
+                
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <a href={resume}> Download My Resume </a>
+                <Link to="/">Home</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+      
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <a href={resume}> Download My Resume </a>
+                <Link to="/">Home</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+   </div> 
+
   );
 }
 
