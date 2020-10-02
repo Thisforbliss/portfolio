6c091818c411905f636c187e3acd6f26dccd6212
Malcolm_Leake <echelon1089@gmail.com>
2020-10-01 21:59:32 -0400

    Updates

---
+++ precache-manifest.f40fe25a677da44d496e0c3d6c7b135f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f9861c137de5b56fe7426eaf98f98b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "05aed7d4443fe5d67f05",
+    "url": "/portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "8f752868ef81a27bea05",
+    "url": "/portfolio/static/css/main.3976d6f9.chunk.css"
+  },
+  {
+    "revision": "05aed7d4443fe5d67f05",
+    "url": "/portfolio/static/js/2.f015db6c.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.f015db6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f752868ef81a27bea05",
+    "url": "/portfolio/static/js/main.89e4fd60.chunk.js"
+  },
+  {
+    "revision": "1ce76c02ee4034b846c7",
+    "url": "/portfolio/static/js/runtime-main.19a89414.js"
+  },
+  {
+    "revision": "230aca6a86a45b8add5e073ac95869db",
+    "url": "/portfolio/static/media/Resume.230aca6a.docx"
+  }
+]);
