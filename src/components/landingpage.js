4a8b4b07a261fd1b54f6c79e17feebc2103fef21
Malcolm_Leake <echelon1089@gmail.com>
2021-04-15 14:51:49 -0400

    working on video sizing

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -18,7 +18,10 @@ class Landing extends Component {
                         <hr/>
                         <p> | Rails | JavaScript | React | Redux | SQL | Ruby | HTML | NodeJs |</p>
 
-                  
+                    <div className= 'elevator-pitch'> 
+                        <iframe title="ad" width="500" height="260" src="https://www.youtube.com/embed/HbcXE9gRHeg" frameBorder="0" 
+                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </div>
 
                     <div className="social-links">
 
@@ -39,10 +42,8 @@ class Landing extends Component {
                         </a>
                     </div> 
 
-                    <div className= 'elevator-pitch'> 
-                        <iframe title="ad" width="500" height="260" src="https://www.youtube.com/embed/HbcXE9gRHeg" frameBorder="0" 
-                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    </div>
+                  
+                    
 
                     </div>
                 </Grid>
